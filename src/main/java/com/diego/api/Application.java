2fd09b4fc4f9feddd2a3cab7959a57063374755d
Diego Nogueira <diegof.fnogueira@gmail.com>
2018-02-12 23:31:44 -0200

    Adicionando controller e objeto DTO

--- src/main/java/com/diego/api/Application.java
+++ src/main/java/com/diego/api/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringDtoApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringDtoApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
---
+++ src/main/java/com/diego/api/controller/EmpresaController.java
@@ -0,0 +1,20 @@
+package com.diego.api.controller;
+
+import com.diego.api.dto.EmpresaDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/empresas")
+public class EmpresaController {
+
+    @PostMapping
+    public ResponseEntity<EmpresaDTO> cadastrar(@RequestBody EmpresaDTO empresaDTO){
+        empresaDTO.setId(1L);
+        return ResponseEntity.ok(empresaDTO);
+    }
+
+}
