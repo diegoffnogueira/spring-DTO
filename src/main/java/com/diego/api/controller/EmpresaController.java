c886eb52d24d63b03f1e235468f6803827862779
Diego Nogueira <diegof.fnogueira@gmail.com>
2018-02-12 23:47:56 -0200

    Criando um Response padrão para o retorno do Controller

--- src/main/java/com/diego/api/controller/EmpresaController.java
+++ src/main/java/com/diego/api/controller/EmpresaController.java
@@ -1,6 +1,7 @@
 package com.diego.api.controller;
 
 import com.diego.api.dto.EmpresaDTO;
+import com.diego.api.response.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,10 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/empresas")
 public class EmpresaController {
 
+//    @PostMapping
+//    public ResponseEntity<EmpresaDTO> cadastrar(@RequestBody EmpresaDTO empresaDTO){
+//        empresaDTO.setId(1L);
+//        return ResponseEntity.ok(empresaDTO);
+//    }
+
     @PostMapping
-    public ResponseEntity<EmpresaDTO> cadastrar(@RequestBody EmpresaDTO empresaDTO){
+    public ResponseEntity<Response<EmpresaDTO>> cadastrar(@RequestBody EmpresaDTO empresaDTO){
+        Response<EmpresaDTO> response = new Response<>();
+
         empresaDTO.setId(1L);
-        return ResponseEntity.ok(empresaDTO);
+        response.setResultado(empresaDTO);
+        return ResponseEntity.ok(response);
     }
 
 }
